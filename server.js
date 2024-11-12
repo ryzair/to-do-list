2e9587fbb1a4ef2f6e6c28f73d3866fd34112ef8
Tyler2450 <tfurtado0311@gmail.com>
2024-11-12 17:03:43 -0500

    Clean Up

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 3000;
 // Connect to MongoDB
 mongoose.connect(DB.URI);
 let mongoDB = mongoose.connection;
-mongoDB.on('error',console.error.bind(console,'Connection Error'));
+mongoDB.on('error',console.error.bind(console,'There is a connection Error'));
 mongoDB.once('open',()=>{
-  console.log("Connected with the MongoDB")
+  console.log("It's connected with MongoDB")
 });
 mongoose.connect(DB.URI,{useNewURIParser:true,useUnifiedTopology:true})
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`The server is running on port ${PORT}`);
 });
